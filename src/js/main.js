2aa32dd099fef003df08f0782e11089c12a424c1
Vadim <hellovados@gmail.com>
2021-04-27 01:08:38 +0300

    add image popup

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
 import timer from './modules/timer';
+import images from './modules/images';
 
 window.addEventListener('DOMContentLoaded', ()=>{
 "use strict";
@@ -16,9 +17,10 @@ let deadline = '2021-08-01';
 changeModalState(modalState);
 modals();
 tabs('.glazing_slider','.glazing_block', '.glazing_content', 'active');
-tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'active');
+tabs('.decoration_slider', '.no_click', '.decoration_content > div > div',  'active');
 tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block')
 forms(modalState);
 timer ('.container1', deadline);
+images();
 });
 
